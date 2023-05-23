5a60a8fa587670373740330f54e246560809fcbd
Esteban <edjeordjian@fi.uba.ar>
2023-05-23 16:30:37 -0300

    Changed link to APK

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -9,6 +9,7 @@ import {
     EVENT_INFO_STR
 } from "../../constants/generalConstants";
 
+
 const Home = () => {
     const eventId = window.location.href.split(EVENT_INFO_STR + "/")[1];
 
@@ -27,11 +28,13 @@ const Home = () => {
 
            <BlankLine/>
 
-           <Typography> ¿Todavía no tenés la aplicación? Descargala acá para Android:
+           <Typography> ¿Todavía no tenés la aplicación?
            </Typography>
 
-           <Typography> https://drive.google.com/drive/folders/1sRYQEzYPe54zNvUkHYNKIk3gs7pJEDfc
-           </Typography>
+           <a href="https://drive.google.com/file/d/1X2-f7p-vsql9BHcl-58Xe2725bHjl-OO"
+              rel="noopener noreferrer"
+           >¡Descargala acá para Android!
+           </a>
        </>
     );
 };
